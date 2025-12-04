ab1d60f89cf7a31cceafca12ba9f56c632fd90b7
Goniix <dragonixgold@gmail.com>
2025-12-04 17:04:17 +0100

    structs

--- TP4.h
+++ TP4.h
@@ -1,1 +1,27 @@
+typedef struct t_Position
+{
+    int numeroLigne;
+    int ordre;
+    int numeroPhrase;
+    struct t_Position *suivant;
+} Position;
 
+typedef struct t_Noeud
+{
+    char *mot;
+    int nbOccurence;
+    Position *listePositions;
+    struct t_Noeud *gauche;
+    struct t_Noeud *droit;
+} Noeud;
+
+typedef struct t_Index
+{
+    Noeud *racine;
+    int nbMotsDistincts;
+    int nbMotsTotal;
+} Index;
+
+Position *init_Position();
+Position *init_Noeud();
+Position *init_Index();
